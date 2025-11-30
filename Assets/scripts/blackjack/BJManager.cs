57be38cd9462849d1d54029d4a6e97d662f066d7
ajlares <jaredlares5@gmail.com>
2025-11-29 20:31:09 -0600

    commit

--- Assets/scripts/blackjack/BJManager.cs
+++ Assets/scripts/blackjack/BJManager.cs
@@ -3,47 +3,59 @@ using UnityEngine;
 
 public class BJManager : MonoBehaviour
 {
-    [Header("       ----- General Setttings -----")]
+    [Header("               ----- General Setttings -----")]
     public List<Card> cards;
     public Stack<Card> cardStack;
-    [Header("       ----- Player Settings-----")]
+    [Header("               ----- Player Settings-----")]
     public List<Transform> PlayerSpawnCardList; 
     public List<Card> PlayeCardList;
-    [Header("       ----- Dealer Settings-----")]
+    public int playerSpawnNumber = 0;
+    [Header("               ----- Dealer Settings-----")]
     public List<Transform> DealerSpawnCardList; 
     public List<Transform> DealerCardList;
-    
     private void Start()
     {
-        RestartGame();
         StartGame();
     }
-
+    
     public void StartGame()
     {
         RestartGame();
         TakeCard();
     }
-
+    
     public void TakeCard()
     {
         Card tempCard = cardStack.Pop();
         GameObject tempGO = new GameObject();
         tempGO.AddComponent<SpriteRenderer>();
         tempGO.GetComponent<SpriteRenderer>().sprite = tempCard.sprite;
-        Instantiate(tempGO, PlayerSpawnCardList[0].position, Quaternion.identity);
+        tempGO.transform.position = PlayerSpawnCardList[playerSpawnNumber].position;
+        if (playerSpawnNumber < PlayerSpawnCardList.Count - 1)
+        {
+            playerSpawnNumber++;
+        }
+    }
+
+    public void hitButton()
+    {
+        TakeCard();
+    }
+
+    public void staryButton()
+    {
+        
     }
     private void RestartGame()
     {
-        shuffleStack();   
+        ShuffleStack();   
     }
-
-    private void shuffleStack()
+    
+    private void ShuffleStack()
     {
         if (cards.Count > 0)
         {
             cardStack = new Stack<Card>();
-            Debug.Log("shuffle cards");
             System.Random rng = new System.Random();
             for (int i = cards.Count - 1; i > 0; i--)
             {
