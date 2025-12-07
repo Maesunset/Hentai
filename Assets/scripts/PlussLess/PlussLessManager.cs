3866f2fee93441b46b3d6e25d1a332c39da3bae1
ajlares <119090809+Maesunset@users.noreply.github.com>
2025-12-06 18:04:31 -0600

    commit

--- Assets/scripts/PlussLess/PlussLessManager.cs
+++ Assets/scripts/PlussLess/PlussLessManager.cs
@@ -5,24 +5,50 @@ public class PlussLessManager : MonoBehaviour
 {
     [Header("Basics")]
     public List<Card> cardList;
+    public GameObject cardHolder;
     public Card previeCard;
     public Card actualCard;
 
     private void Start()
     { 
         int randomPrevieCard = Random.Range(0, cardList.Count);
-        previeCard = cardList[randomPrevieCard];
-        Debug.Log(previeCard.name);
+        actualCard = cardList[randomPrevieCard];
+        Debug.Log(actualCard.name);
+        cardHolder.GetComponent<SpriteRenderer>().sprite = actualCard.sprite;
         
+    }
+
+    private void Reroll()
+    {
+           previeCard = actualCard;
+           int randomPrevieCard = Random.Range(0, cardList.Count);
+           actualCard = cardList[randomPrevieCard];
+           cardHolder.GetComponent<SpriteRenderer>().sprite = actualCard.sprite;
     }
 
     public void Plus()
     {
-        
+        Reroll();
+        if (actualCard.Value > previeCard.Value)
+        {
+            Debug.Log("win");
+        }
+        else
+        {
+            Debug.Log("Lose");
+        }
     }
 
     public void Less()
     {
-        
+        Reroll();
+        if (actualCard.Value < previeCard.Value)
+        {
+            Debug.Log("win");
+        }
+        else
+        {
+            Debug.Log("Lose");
+        }
     }
 }
