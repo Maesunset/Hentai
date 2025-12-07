ae019d88878ec56edcb575a94f8313edf8c19dd1
ajlares <119090809+Maesunset@users.noreply.github.com>
2025-12-07 02:45:09 -0600

    addBasicSounds

---
+++ Assets/scripts/SceneManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    public void MainMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+
+    public void BlackJack()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("BlackJack");
+    }    
+    public void PlusAndLess()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("PlusAndLess");
+    }    
+    public void JackPot()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("JackPot");
+    }    
+    public void Slot()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Slot");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
