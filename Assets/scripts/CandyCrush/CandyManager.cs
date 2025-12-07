5a706bdbfc5b9a910ec1f15297dc58518de0fcdb
ajlares <119090809+Maesunset@users.noreply.github.com>
2025-12-06 19:49:14 -0600

    secondGame

--- Assets/scripts/CandyCrush/CandyManager.cs
+++ Assets/scripts/CandyCrush/CandyManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -8,24 +9,29 @@ public class CandyManager : MonoBehaviour
     public List<Sprite> candySprites;
     public GameObject candyPrefab;
     private List<List<GameObject>> candysList;
+    public List<GameObject> SpawnPoints;
+    public int heigth;
 
-    private void Start()
+    IEnumerator spawnCandy()
     {
+        for (int i = 0; i < heigth; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                int randomSprite = Random.Range(0, candySprites.Count);
+                GameObject Temp = candyPrefab;
+                Temp.GetComponent<SpriteRenderer>().sprite = candySprites[randomSprite];
+                Temp.GetComponent<CandyType>().icon = GetEnum(randomSprite);
+                Instantiate(candyPrefab, SpawnPoints[j].transform.position, Quaternion.identity);
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+        yield return null;
         
-    }
-
-    public void Spawn()
-    {
-        int randomSprite = Random.Range(0, candySprites.Count);
-        GameObject Temp = candyPrefab;
-        Temp.GetComponent<SpriteRenderer>().sprite = candySprites[randomSprite];
-        Temp.GetComponent<CandyType>().icon = GetEnum(randomSprite);
-        Debug.Log(randomSprite);
-        Debug.Log(Temp.GetComponent<CandyType>().icon);
     }
     public void Reroll()
     {
-        Spawn();
+        StartCoroutine(spawnCandy());
     }
 
     public IconType GetEnum(int enumIndex)
